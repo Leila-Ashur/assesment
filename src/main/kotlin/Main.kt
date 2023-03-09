ecee8917a908fbb44ef147d20beb3389aaefe7cf
leilaashur <leilahashur@gmail.com>
2023-03-09 09:00:10 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,38 @@
+fun main() {
+var two=names("leila","ashur")
+    school("Anitab")
+    addition(3,7)
+}
+fun names(x:String,y:String):String{
+    var student= ("x==y")
+    return ("${student[0]}")
+
+}fun numbers(num1:Int,num2:Int,num3:Int){
+    var numb = arrayOf(num1,num2,num3)
+    return
+
+
+
+
+}
+fun school(classes:String){
+
+    println(classes)
+}
+fun cities(kigali:String ){
+
+}
+class calculater(var addition:String, var subtraction:String,var accurate:Double,var multiplication:Int)
+fun addition(num1:Int,num2:Int){
+    var num = num1 +num2
+    println(num)
+
+
+
+}
+fun subtraction(num1:Int,num2:Int){
+    var num = (num1-num2)
+
+}
+fun division()
+
